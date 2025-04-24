ec8659e0f1708b188061dbf2d795c14701dfdafd
Samuel George <samuel.george@thirdwayv.com>
2025-04-24 14:00:51 +0200

    CRYP-4228: Update onTransaction and onSignature Insights

--- src/index.ts
+++ src/index.ts
@@ -13,9 +13,8 @@ import { getState } from './stateManagement';
 
 import type { OnTransactionHandler } from '@metamask/snaps-sdk';
 import type { OnSignatureHandler } from "@metamask/snaps-sdk";
-import { SeverityLevel, panel, text, row, address } from '@metamask/snaps-sdk';
+import { SeverityLevel, panel, text } from '@metamask/snaps-sdk';
 import { remove0x } from '@metamask/utils';
-import { hasProperty } from '@metamask/utils';
 /**
  * The function signatures for the different types of transactions. This is used
  * to determine the type of transaction. This list is not exhaustive, and only
@@ -70,82 +69,31 @@ async function getKeyring(): Promise<SimpleKeyring> {
   return keyring;
 }
 
+
 /**
- * Handle incoming signature requests, sent through one of the following methods:
- * `personal_sign`, `eth_signTypedData`, `eth_signTypedData_v3`, `eth_signTypedData_v4`.
- * The `onSignature` handler is different from the `onRpcRequest` handler in
- * that it is called by MetaMask when a signature request is initiated, rather than
- * when a dapp sends a JSON-RPC request. The handler is called before the
- * signature is made, so it can be used to display information about the
- * signature request to the user before they sign.
  * The `onSignature` handler returns a Snaps UI component, which is displayed
  * in the signature insights panel.
- * @param args - The request parameters.
- * @param args.signature - The signature object. This contains the
  * @returns The signature insights.
  */
-export const onSignature: OnSignatureHandler = async ({ signature }) => {
+export const onSignature: OnSignatureHandler = async ({ }) => {
 
-  const { signatureMethod, from, data } = signature;
-
-  switch (signatureMethod) {
-    case 'personal_sign':
-      return {
-        content: panel([row('From:', text(from)), row('Data:', text(data))]),
-        severity: SeverityLevel.Critical,
-      };
-
-    case 'eth_sign':
-    case 'eth_signTypedData':
-    case 'eth_signTypedData_v3':
-    case 'eth_signTypedData_v4':
-      throw new Error(`UnSupported signature method: ${signatureMethod}`);
-    default:
-      throw new Error(`UnSupported signature method`);
-  }
+  return {
+    content: panel([text('Press "Confirm," then click "Go to website" to review and sign the message on your wallet device.')]),
+    severity: SeverityLevel.Critical,
+  };
 };
 
-
-
 /**
- * Handle incoming transactions, sent through the `wallet_sendTransaction`
- * method. This handler decodes the transaction data, and displays the type of
- * transaction in the transaction insights panel.
- * The `onTransaction` handler is different from the `onRpcRequest` handler in
- * that it is called by MetaMask when a transaction is initiated, rather than
- * when a dapp sends a JSON-RPC request. The handler is called before the
- * transaction is signed, so it can be used to display information about the
- * transaction to the user before they sign it.
  * The `onTransaction` handler returns a Snaps UI component, which is displayed
  * in the transaction insights panel.
- * @param args - The request parameters.
- * @param args.transaction - The transaction object. This contains the
- * transaction parameters, such as the `from`, `to`, `value`, and `data` fields.
  * @returns The transaction insights.
  */
-export const onTransaction: OnTransactionHandler = async ({ transaction }) => {
+export const onTransaction: OnTransactionHandler = async ({ }) => {
 
-  if (
-    hasProperty(transaction, 'data') &&
-    typeof transaction.data === 'string'
-  ) {
-    const type = decodeData(transaction.data);
-    return {
-      content: panel([
-        row('From', address(transaction.from as `0x${string}`)),
-        row(
-          'To',
-          transaction.to
-            ? address(transaction.to as `0x${string}`)
-            : text('None'),
-        ),
-        row('Transaction type', text(type)),
-      ]),
-      severity: SeverityLevel.Critical,
-    };
-  }
-
-  return { content: panel([row('Transaction type', text('Unknown'))]) };
+  return {
+    content: panel([text('Press "Confirm," then click "Go to website" to review and sign the transaction on your wallet device.')]),
+    severity: SeverityLevel.Critical,
+  };
 };
 
 /**
